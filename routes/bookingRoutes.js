2e6673484e699ea72f0f9b15f2d3064a92935300
chiragparihar7 <chiragparihar118@gmail.com>
2025-11-14 17:19:24 +0530

    changes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -6,11 +6,11 @@ import {
   updateBookingStatus,
   getMyBookings,
 } from "../controllers/bookingController.js";
-import { protect, adminAuth, optionalAuth  } from "../middleware/authMiddleware.js";
+import { protect, adminAuth, optionalAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// ✅ Allow both guest and logged-in users
+// ✅ Guests + Logged-in Users can create booking
 router.post("/", optionalAuth, createBooking);
 
 // ✅ Logged-in user's bookings
@@ -18,7 +18,11 @@ router.get("/my", protect, getMyBookings);
 
 // ✅ Admin routes
 router.get("/", adminAuth, getAllBookings);
-router.get("/:id", adminAuth, getBookingById);
+
+// 🔥 Public route (IMPORTANT for booking success page)
+router.get("/:id", getBookingById);
+
+// Admin update booking status
 router.put("/:id/status", adminAuth, updateBookingStatus);
 
 export default router;
